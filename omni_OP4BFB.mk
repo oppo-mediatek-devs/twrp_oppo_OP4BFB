2ff897b92615821807470f8b99c5d866494aa6b8
Emmanuel <alzadidine33@gmail.com>
2024-05-29 09:26:31 +0000

    OP4BFB: Update some files for OP4BFB
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- omni_OP4BFB.mk
+++ omni_OP4BFB.mk
@@ -6,7 +6,8 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -27,4 +28,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Dimen
 TARGET_SCREEN_HEIGHT := 1520
 TARGET_SCREEN_WIDTH := 720
-TW_THEME := portrait_hdpi
